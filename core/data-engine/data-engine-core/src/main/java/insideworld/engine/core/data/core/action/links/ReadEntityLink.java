f329c7e8d220d71ca58d9c7a9213df0d39bf1790
InsideWorld <elish1024@gmail.com>
2022-12-30 22:44:25 +0300

    Reowking action engine

--- core/data-engine/data-engine-core/src/main/java/insideworld/engine/core/data/core/action/links/ReadEntityLink.java
+++ core/data-engine/data-engine-core/src/main/java/insideworld/engine/core/data/core/action/links/ReadEntityLink.java
@@ -1,130 +1,130 @@
-///*
-// * Copyright (c) 2022 Anton Eliseev
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// * associated documentation files (the "Software"), to deal in the Software without restriction,
-// * including without limitation the rights to use, copy, modify, merge, publish, distribute,
-// * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
-// * is furnished to do so, subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all copies or
-// * substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-// * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//
-//package insideworld.engine.core.data.core.action.links;
-//
-//import insideworld.engine.core.action.chain.Link;
-//import insideworld.engine.core.action.chain.LinkException;
-//import insideworld.engine.core.common.exception.CommonException;
-//import insideworld.engine.core.data.core.Entity;
-//import insideworld.engine.core.data.core.StorageException;
-//import insideworld.engine.core.data.core.storages.Storage;
-//import insideworld.engine.core.data.core.storages.keeper.StorageKeeper;
-//import java.util.Collection;
-//import java.util.function.BiConsumer;
-//import java.util.function.Function;
-//import javax.enterprise.context.Dependent;
-//import javax.inject.Inject;
-//import org.apache.commons.collections4.CollectionUtils;
-//import org.apache.commons.lang3.tuple.Pair;
-//
-///**
-// * Read an entity by presents tags.
-// * This is link required to init.
-// * Need to set one or both tags - for multiple and single entity. Also need to set a type.
-// * In case if ID won't present in context:
-// * - For single tag just skip filling.
-// * - For multiple tag will read all entities.
-// * All results will put in context.
-// *
-// * @param <T> Entity type.
-// * @since 0.0.1
-// */
-//@Dependent
-//public class ReadEntityLink<T extends Entity, I> implements Link<I> {
-//
-//    /**
-//     * Storage keeper.
-//     */
-//    private final StorageKeeper storages;
-//
-//    /**
-//     * Storage for provided type.
-//     */
-//    private Storage<T> storage;
-//
-//    private Pair<Function<I, Long>, BiConsumer<T, I>> single;
-//
-//    private Pair<Function<I, Collection<Long>>, BiConsumer<Collection<T>, I>> multiple;
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param storages Storage keeper instance.
-//     */
-//    @Inject
-//    public ReadEntityLink(final StorageKeeper storages) {
-//        this.storages = storages;
-//    }
-//
-//    @Override
-//    public boolean process(final I input) throws CommonException {
-//        if (this.single != null) {
-//            final Long id = this.single.getLeft().apply(input);
-//            if (id != null) {
-//                this.single.getRight().accept(
-//                    this.storage.read(id),
-//                    input
-//                );
-//            }
-//        }
-//        if (this.multiple != null) {
-//            final Collection<Long> ids = this.multiple.getLeft().apply(input);
-//            if (CollectionUtils.isEmpty(ids)) {
-//                this.multiple.getRight().accept(this.storage.readAll(), input);
-//            } else {
-//                this.multiple.getRight().accept(this.storage.read(ids), input);
-//            }
-//        }
-//        return true;
-//    }
-//
-//    /**
-//     * Set type of entity.
-//     *
-//     * @param type Entity type.
-//     * @return The same instance
-//     * @throws StorageException Can't find storage.
-//     */
-//    public ReadEntityLink<T, I> setType(final Class<T> type) throws StorageException {
-//        this.storage = this.storages.getStorage(type);
-//        return this;
-//    }
-//
-//    public ReadEntityLink<T, I> setSingle(
-//        final Function<I, Long> ids, final BiConsumer<T, I> result
-//    ) throws LinkException {
-//        if (ids == null || result == null) {
-//            throw new LinkException(this, "One or both arguments is null");
-//        }
-//        this.single = Pair.of(ids, result);
-//        return this;
-//    }
-//
-//    public ReadEntityLink<T, I> setMultiple(
-//        final Function<I, Collection<Long>> ids,
-//        final BiConsumer<Collection<T>, I> results
-//    ) throws LinkException {
-//        if (ids == null || results == null) {
-//            throw new LinkException(this, "One or both arguments is null");
-//        }
-//        this.multiple = Pair.of(ids, results);
-//        return this;
-//    }
-//}
+/*
+ * Copyright (c) 2022 Anton Eliseev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package insideworld.engine.core.data.core.action.links;
+
+import insideworld.engine.core.action.chain.Link;
+import insideworld.engine.core.action.chain.LinkException;
+import insideworld.engine.core.common.exception.CommonException;
+import insideworld.engine.core.data.core.Entity;
+import insideworld.engine.core.data.core.StorageException;
+import insideworld.engine.core.data.core.storages.Storage;
+import insideworld.engine.core.data.core.storages.keeper.StorageKeeper;
+import java.util.Collection;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ * Read an entity by presents tags.
+ * This is link required to init.
+ * Need to set one or both tags - for multiple and single entity. Also need to set a type.
+ * In case if ID won't present in context:
+ * - For single tag just skip filling.
+ * - For multiple tag will read all entities.
+ * All results will put in context.
+ *
+ * @param <T> Entity type.
+ * @since 0.0.1
+ */
+@Dependent
+public class ReadEntityLink<T extends Entity, I> implements Link<I> {
+
+    /**
+     * Storage keeper.
+     */
+    private final StorageKeeper storages;
+
+    /**
+     * Storage for provided type.
+     */
+    private Storage<T> storage;
+
+    private Pair<Function<I, Long>, BiConsumer<T, I>> single;
+
+    private Pair<Function<I, Collection<Long>>, BiConsumer<Collection<T>, I>> multiple;
+
+    /**
+     * Constructor.
+     *
+     * @param storages Storage keeper instance.
+     */
+    @Inject
+    public ReadEntityLink(final StorageKeeper storages) {
+        this.storages = storages;
+    }
+
+    @Override
+    public boolean process(final I input) throws CommonException {
+        if (this.single != null) {
+            final Long id = this.single.getLeft().apply(input);
+            if (id != null) {
+                this.single.getRight().accept(
+                    this.storage.read(id),
+                    input
+                );
+            }
+        }
+        if (this.multiple != null) {
+            final Collection<Long> ids = this.multiple.getLeft().apply(input);
+            if (CollectionUtils.isEmpty(ids)) {
+                this.multiple.getRight().accept(this.storage.readAll(), input);
+            } else {
+                this.multiple.getRight().accept(this.storage.read(ids), input);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Set type of entity.
+     *
+     * @param type Entity type.
+     * @return The same instance
+     * @throws StorageException Can't find storage.
+     */
+    public ReadEntityLink<T, I> setType(final Class<T> type) throws StorageException {
+        this.storage = this.storages.getStorage(type);
+        return this;
+    }
+
+    public ReadEntityLink<T, I> setSingle(
+        final Function<I, Long> ids, final BiConsumer<T, I> result
+    ) throws LinkException {
+        if (ids == null || result == null) {
+            throw new LinkException(this, "One or both arguments is null");
+        }
+        this.single = Pair.of(ids, result);
+        return this;
+    }
+
+    public ReadEntityLink<T, I> setMultiple(
+        final Function<I, Collection<Long>> ids,
+        final BiConsumer<Collection<T>, I> results
+    ) throws LinkException {
+        if (ids == null || results == null) {
+            throw new LinkException(this, "One or both arguments is null");
+        }
+        this.multiple = Pair.of(ids, results);
+        return this;
+    }
+}
--- core/data-engine/data-engine-core/src/main/java/insideworld/engine/core/data/core/action/links/WriteEntityLink.java
+++ core/data-engine/data-engine-core/src/main/java/insideworld/engine/core/data/core/action/links/WriteEntityLink.java
@@ -1,177 +1,142 @@
-///*
-// * Copyright (c) 2022 Anton Eliseev
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// * associated documentation files (the "Software"), to deal in the Software without restriction,
-// * including without limitation the rights to use, copy, modify, merge, publish, distribute,
-// * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
-// * is furnished to do so, subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all copies or
-// * substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-// * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//
-//package insideworld.engine.core.data.core.action.links;
-//
-//import insideworld.engine.core.action.chain.Link;
-//import insideworld.engine.core.action.chain.LinkException;
-//import insideworld.engine.core.action.keeper.Record;
-//import insideworld.engine.core.action.keeper.context.Context;
-//import insideworld.engine.core.action.keeper.output.Output;
-//import insideworld.engine.core.data.core.Entity;
-//import insideworld.engine.core.data.core.StorageException;
-//import insideworld.engine.core.data.core.storages.Storage;
-//import insideworld.engine.core.data.core.storages.keeper.StorageKeeper;
-//import insideworld.engine.core.data.core.tags.EntitiesTag;
-//import insideworld.engine.core.data.core.tags.EntityTag;
-//import java.util.Collection;
-//import javax.enterprise.context.Dependent;
-//import javax.inject.Inject;
-//import org.apache.commons.collections4.CollectionUtils;
-//
-///**
-// * Write entities link.
-// * Using to call write method in storage for specific entity or entities from context.
-// * This is link required to init. See setTag and count method comments.
-// *
-// * @param <T> Entity type to delete.
-// * @since 0.0.1
-// */
-//@Dependent
-//public class WriteEntityLink<T extends Entity> implements Link {
-//
-//    /**
-//     * Storage keeper.
-//     */
-//    private final StorageKeeper storages;
-//
-//    /**
-//     * Tag for single entity.
-//     */
-//    private EntityTag<T> single;
-//
-//    /**
-//     * Tag for multiple entity.
-//     */
-//    private EntitiesTag<T> multiple;
-//
-//    /**
-//     * Write count of write in output.
-//     * Looks like need to delete it in the future.
-//     */
-//    private boolean count;
-//
-//    /**
-//     * Default constructor.
-//     *
-//     * @param storages Storage keeper.
-//     */
-//    @Inject
-//    public WriteEntityLink(final StorageKeeper storages) {
-//        this.storages = storages;
-//    }
-//
-//    @Override
-//    public final void process(final Context context, final Output output)
-//        throws LinkException, StorageException {
-//        if (this.single == null && this.multiple == null) {
-//            throw new LinkException(this, "Link was not init");
-//        }
-//        context.put(
-//            this.single, this.writeSingle(context.get(this.single), output), true
-//        );
-//        context.put(
-//            this.multiple, this.writeMultiple(context.get(this.multiple), output), true
-//        );
-//    }
-//
-//    /**
-//     * Set tag of entity which need to write.
-//     *
-//     * @param tag Entity tag.
-//     * @return The same instance.
-//     */
-//    public WriteEntityLink<T> setTag(final EntityTag<T> tag) {
-//        this.single = tag;
-//        return this;
-//    }
-//
-//    /**
-//     * Set tag of entities which need to write.
-//     *
-//     * @param tag Entities tag.
-//     * @return The same instance.
-//     */
-//    public WriteEntityLink<T> setTag(final EntitiesTag<T> tag) {
-//        this.multiple = tag;
-//        return this;
-//    }
-//
-//    /**
-//     * Create a record in output with count of written entities.
-//     *
-//     * @return The same instance.
-//     */
-//    public WriteEntityLink<T> setCount() {
-//        this.count = true;
-//        return this;
-//    }
-//
-//    /**
-//     * Write single entity.
-//     *
-//     * @param entity Entity.
-//     * @param output Output.
-//     * @return Persisted entity or null.
-//     * @throws StorageException Can't write entity.
-//     */
-//    private T writeSingle(final T entity, final Output output) throws StorageException {
-//        final T result;
-//        if (entity == null) {
-//            result = null;
-//        } else {
-//            final Storage<T> storage = (Storage<T>) this.storages.getStorage(entity.getClass());
-//            result = storage.write(entity);
-//            if (this.count) {
-//                final Record record = output.createRecord();
-//                record.put("type", this.single.getTag());
-//                record.put("count", 1);
-//            }
-//        }
-//        return result;
-//    }
-//
-//    /**
-//     * Write entities.
-//     *
-//     * @param collection Collection of entities.
-//     * @param output Output.
-//     * @return Persisted entities or empty collection.
-//     * @throws StorageException Can't write entities.
-//     */
-//    private Collection<T> writeMultiple(final Collection<T> collection, final Output output)
-//        throws StorageException {
-//        final Collection<T> result;
-//        if (CollectionUtils.isEmpty(collection)) {
-//            result = null;
-//        } else {
-//            final Storage<T> storage = (Storage<T>) this.storages.getStorage(
-//                collection.iterator().next().getClass()
-//            );
-//            result = storage.writeAll(collection);
-//            if (this.count) {
-//                final Record record = output.createRecord();
-//                record.put("type", this.multiple.getTag());
-//                record.put("count", result.size());
-//            }
-//        }
-//        return result;
-//    }
-//
-//}
+/*
+ * Copyright (c) 2022 Anton Eliseev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package insideworld.engine.core.data.core.action.links;
+
+import insideworld.engine.core.action.chain.Link;
+import insideworld.engine.core.action.chain.LinkException;
+import insideworld.engine.core.data.core.Entity;
+import insideworld.engine.core.data.core.StorageException;
+import insideworld.engine.core.data.core.storages.Storage;
+import insideworld.engine.core.data.core.storages.keeper.StorageKeeper;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+/**
+ * Write entities link.
+ * Using to call write method in storage for specific entity or entities from context.
+ * This is link required to init. See setTag and count method comments.
+ *
+ * @param <T> Entity type to delete.
+ * @since 0.0.1
+ */
+@Dependent
+public class WriteEntityLink<T extends Entity, I> implements Link<I> {
+
+    /**
+     * Storage keeper.
+     */
+    private final StorageKeeper storages;
+
+    /**
+     * Tag for single entity.
+     */
+    private Pair<Function<I, T>, BiConsumer<I, T>> single;
+
+    /**
+     * Tag for multiple entity.
+     */
+    private Pair<Function<I, Collection<T>>, BiConsumer<I, Collection<T>>> multiple;
+
+    private Storage<T> storage;
+
+    /**
+     * Default constructor.
+     *
+     * @param storages Storage keeper.
+     */
+    @Inject
+    public WriteEntityLink(final StorageKeeper storages) {
+        this.storages = storages;
+    }
+
+    @Override
+    public final boolean process(final I input) throws LinkException, StorageException {
+        if (this.single == null && this.multiple == null) {
+            throw new LinkException(this, "Link was not init");
+        }
+        //TODO: Complexity!
+        if (this.single != null) {
+            final T entity = this.single.getLeft().apply(input);
+            if (entity != null) {
+                final T write = this.storage.write(entity);
+                Optional.of(this.single.getRight()).ifPresent(handler -> handler.accept(input, write));
+            }
+        }
+        if (this.multiple != null) {
+            final Collection<T> entity = this.multiple.getLeft().apply(input);
+            if (entity != null) {
+                final Collection<T> write = this.storage.writeAll(entity);
+                Optional.of(this.multiple.getRight())
+                        .ifPresent(handler -> handler.accept(input, write));
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Set tag of entity which need to write.
+     *
+     * @param tag Entity tag.
+     * @return The same instance.
+     */
+    public WriteEntityLink<T, I> setSingle(
+            final Function<I, T> entity,
+            final BiConsumer<I, T> handler
+    ) {
+        this.single = Pair.of(entity, handler);
+        return this;
+    }
+
+    /**
+     * Set tag of entities which need to write.
+     *
+     * @param tag Entities tag.
+     * @return The same instance.
+     */
+    public WriteEntityLink<T, I> setMultiple(
+            final Function<I, Collection<T>> entities,
+            final BiConsumer<I, Collection<T>> handler
+    ) {
+        this.multiple = Pair.of(entities, handler);
+        return this;
+    }
+
+    /**
+     * Set type of entity.
+     * It's necessary to define which storage need to use.
+     *
+     * @param type Entity type.
+     * @return The same instance
+     * @throws StorageException If storage can't find for provided type.
+     */
+    public final WriteEntityLink<T, I> setType(final Class<T> type) throws StorageException {
+        this.storage = this.storages.getStorage(type);
+        return this;
+    }
+}
